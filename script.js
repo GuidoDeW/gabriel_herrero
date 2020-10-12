3fd15907e19cee2a695533b685669872272e5207
Guido de Wilde <guido_dewilde@hotmail.com>
2020-10-12 19:08:09 +0200

    initial dropdown styling with JS - regroup CSS props

---
+++ script.js
@@ -0,0 +1,19 @@
+const dropdownMenu = document.getElementById("dropdown-menu"),
+  hamburgerBtn = document.getElementById("hamburger-btn");
+
+
+document.body.addEventListener("click", (e)=> {
+  if(dropdownMenu.classList.contains("show") && !(e.target.classList.contains("hamburger-btn")) && !(e.target.classList.contains("navlink")))  {
+    dropdownMenu.classList.remove("show");
+    dropdownMenu.classList.add("hide");
+    hamburgerBtn.classList.remove("open");
+  };
+});
+
+hamburgerBtn.addEventListener("click", ()=> {
+  dropdownMenu.classList.toggle("hide");
+  dropdownMenu.classList.toggle("show");
+  hamburgerBtn.classList.toggle("open");
+  // dropdownMenu.classList.contains("show") ? hamburgerBtn.classList.add("open") : hamburgerBtn.classList.remove("open");
+})
+
