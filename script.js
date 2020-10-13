69fecc846e0e6560aeede9f0f71e5dd2cce58db4
Guido de Wilde <guido_dewilde@hotmail.com>
2020-10-13 14:10:41 +0200

    improve nav transitions - solve invisible link pointer - clean up redundant styles

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const dropdownMenu = document.getElementById("dropdown-menu"),
 document.body.addEventListener("click", (e)=> {
   if(dropdownMenu.classList.contains("show") && !(e.target.classList.contains("hamburger-btn")) && !(e.target.classList.contains("navlink")))  {
     dropdownMenu.classList.remove("show");
-    dropdownMenu.classList.add("hide");
     hamburgerBtn.classList.remove("fa-times")
     hamburgerBtn.classList.add("fa-bars");
     hamburgerBtn.classList.remove("open");
@@ -14,7 +13,6 @@ document.body.addEventListener("click", (e)=> {
 });
 
 hamburgerBtn.addEventListener("click", ()=> {
-  dropdownMenu.classList.toggle("hide");
   dropdownMenu.classList.toggle("show");
   hamburgerBtn.classList.toggle("fa-bars");
   hamburgerBtn.classList.toggle("fa-times");
