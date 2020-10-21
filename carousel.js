406b371d4cd77569b4ce2b892a29de58f178e406
Guido de Wilde <guido_dewilde@hotmail.com>
2020-10-21 17:27:44 +0200

    init carousel styling and script

---
+++ carousel.js
@@ -0,0 +1,77 @@
+const galleryItems = document.querySelectorAll(".gallery-item"),
+imgSources = Array.from(document.querySelectorAll(".gallery-img")).map((img) => { return img.attributes.src.value}),
+gallery = document.getElementById("gallery"),
+carousel = document.getElementById("img-carousel"),
+carouselImg = document.getElementById("carousel-img"),
+prevBtn = document.getElementById("carousel-prev-btn"),
+nextBtn = document.getElementById("carousel-next-btn"),
+closeBtn = document.getElementById("carousel-close-btn");
+
+const galleryIndex = (function () {
+  let currentImage = 0;
+  return {
+    set: (num) => {
+      currentImage = num;
+    },
+    decrease: () => {
+      currentImage--;
+    },
+    increase: () => {
+      currentImage++;
+    },
+    reset: () => {
+      currentImage = 0;
+    },
+    current: () => {
+      return currentImage;
+    }
+  }
+})();
+
+function nextImg () {
+  galleryIndex.current() >  0 ? galleryIndex.decrease() : galleryIndex.set(imgSources.length - 1);
+  carouselImg.setAttribute("src", imgSources[galleryIndex.current()]);
+}
+
+function prevImg() {
+  galleryIndex.current() <  imgSources.length - 1 ? galleryIndex.increase() : galleryIndex.reset();
+  carouselImg.setAttribute("src", imgSources[galleryIndex.current()]);
+}
+
+function openCarousel() {
+  gallery.classList.add("carousel-show");
+  carousel.classList.add("carousel-show");
+}
+
+function closeCarousel() {
+  carousel.classList.remove("carousel-show");
+  gallery.classList.remove("carousel-show");
+}
+
+document.body.addEventListener("keydown", (e) => {
+  if(carousel.classList.contains("carousel-show")) {
+    if(e.key === 39 || e.keyCode === 39) {
+      nextImg();
+    } 
+
+    if(e.key === 37 || e.keyCode === 37) {
+      prevImg();
+    }
+
+    if(e.key === 27 || e.keyCode === 27) {
+      closeCarousel();
+    }
+  }
+});
+
+closeBtn.addEventListener("click", closeCarousel);
+
+prevBtn.addEventListener("click", nextImg);
+
+nextBtn.addEventListener("click", prevImg);
+
+galleryItems.forEach((item, index) => item.querySelector(".gallery-item-text").addEventListener("click", ()=> {
+  openCarousel();
+  galleryIndex.set(index);
+  carouselImg.setAttribute("src", imgSources[galleryIndex.current()]);
+}));
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const dropdownMenu = document.getElementById("dropdown-menu"),
   hamburgerBtn = document.getElementById("hamburger-btn"),
   exploreBtn = document.getElementById("explore-btn");
 
-
 document.body.addEventListener("click", (e)=> {
   if(dropdownMenu.classList.contains("show") && !(e.target.classList.contains("hamburger-btn")) && !(e.target.classList.contains("navlink")))  {
     dropdownMenu.classList.remove("show");
