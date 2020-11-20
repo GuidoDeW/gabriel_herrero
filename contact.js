a1a8f26333536b787eb79ec82a85476702729bef
Guido de Wilde <guido_dewilde@hotmail.com>
2020-11-20 15:46:30 +0100

    form submit check and LS fill-out - new text-align classes

--- contact.js
+++ contact.js
@@ -1,6 +1,73 @@
 const submitBtn = document.getElementById("contact-submit-btn"),
   inputFields = document.querySelectorAll(".input-field");
 
+inputFields.forEach((field) => {
+  // Check LS and fill previously filled-out fields
+  if (
+    localStorage.getItem(`input-${[...inputFields].indexOf(field)}`) !== null
+  ) {
+    field.value = JSON.parse(
+      localStorage.getItem(`input-${[...inputFields].indexOf(field)}`)
+    );
+  }
+
+  // Store field input to LS
+  field.addEventListener("input", () => {
+    localStorage.setItem(
+      `input-${[...inputFields].indexOf(field)}`,
+      JSON.stringify(field.value)
+    );
+  });
+
+  // Wipe invalid input and error msg on focus
+  field.addEventListener("focus", (e) => {
+    if (e.target.classList.contains("incorrect-field")) {
+      e.target.classList.remove("incorrect-field");
+      e.target.setAttribute("placeholder", "");
+    }
+  });
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let incorrectFields = [];
+  //Check required fields
+  inputFields.forEach((field) => {
+    if (
+      field.classList.contains("required-field") &&
+      field.value.trim().length === 0
+    ) {
+      incorrectFields.push(field);
+      field.setAttribute("placeholder", "Please fill out this field.");
+    } else if (
+      field.name === "email" &&
+      !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(field.value)
+    ) {
+      incorrectFields.push(field);
+      field.setAttribute("placeholder", "Please enter a valid email address.");
+    }
+  });
+
+  if (incorrectFields.length > 0) {
+    // Wipe fields with invalid input and show error msg
+    incorrectFields.forEach((field) => {
+      field.classList.add("incorrect-field");
+      field.value = "";
+    });
+  } else {
+    // Remove LS items upon successful submit (without affecting LS items from other apps)
+    inputFields.forEach((field) => {
+      if (
+        localStorage.getItem(`input-${[...inputFields].indexOf(field)}`) !==
+        null
+      ) {
+        localStorage.removeItem(`input-${[...inputFields].indexOf(field)}`);
+      }
+    });
+    alert("Thank you!");
+  }
+});
+
 // use regexs to check for correct input. Replace required attribute
 // with class (or alter behaviour of required input fields) to display
 // red text inside empty required fields upon submit.
