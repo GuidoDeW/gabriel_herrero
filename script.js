e793be027ca5e78b1be3431bdcfdb7ac5bee4af5
Guido de Wilde <guido_dewilde@hotmail.com>
2020-12-07 17:05:29 +0100

    select all in-page hrefs with JS

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const dropdownMenu = document.getElementById("dropdown-menu"),
   hamburgerBtn = document.getElementById("hamburger-btn"),
-  exploreBtn = document.getElementById("explore-btn");
+  exploreBtn = document.getElementById("explore-btn"),
+  inPageLinks = document.querySelectorAll("a"),
+  inPageDestinations = [];
 
-document.body.addEventListener("click", (e)=> {
-  if(dropdownMenu.classList.contains("show") && !(e.target.classList.contains("hamburger-btn")) && !(e.target.classList.contains("navlink")))  {
-    dropdownMenu.classList.remove("show");
-    hamburgerBtn.classList.remove("fa-times")
-    hamburgerBtn.classList.add("fa-bars");
-    hamburgerBtn.classList.remove("open");
-  };
+inPageLinks.forEach((link) => {
+  if (document.getElementById(`${link.getAttribute("href").substring(1)}`)) {
+    inPageDestinations.push(link.getAttribute("href"));
+  }
 });
 
-hamburgerBtn.addEventListener("click", ()=> {
+document.body.addEventListener("click", (e) => {
+  if (
+    dropdownMenu.classList.contains("show") &&
+    !e.target.classList.contains("hamburger-btn") &&
+    !e.target.classList.contains("navlink")
+  ) {
+    dropdownMenu.classList.remove("show");
+    hamburgerBtn.classList.remove("fa-times");
+    hamburgerBtn.classList.add("fa-bars");
+    hamburgerBtn.classList.remove("open");
+  }
+});
+
+hamburgerBtn.addEventListener("click", () => {
   dropdownMenu.classList.toggle("show");
   hamburgerBtn.classList.toggle("fa-bars");
   hamburgerBtn.classList.toggle("fa-times");
   hamburgerBtn.classList.toggle("open");
 });
-
